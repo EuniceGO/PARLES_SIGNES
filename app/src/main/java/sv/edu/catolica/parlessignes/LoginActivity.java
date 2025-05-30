6e70f5271b3da5081934007624b676efc6cd9ba5
Carolina Zepeda <mirian.hernandez1@catolica.edu.sv>
2025-05-30 01:37:18 -0600

    Enviando cambios de los activity de java

--- app/src/main/java/sv/edu/catolica/parlessignes/LoginActivity.java
+++ app/src/main/java/sv/edu/catolica/parlessignes/LoginActivity.java
@@ -4,6 +4,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 
+import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
@@ -13,6 +14,7 @@ public class LoginActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
         setContentView(R.layout.login);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
