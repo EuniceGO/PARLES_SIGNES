42d22b1e9add19ce92ae6987dd3b02538255273c
Carolina Zepeda <mirian.hernandez1@catolica.edu.sv>
2025-05-30 01:24:45 -0600

    Enviando cambios de los activity de java

--- app/src/main/java/sv/edu/catolica/parlessignes/Registro.java
+++ app/src/main/java/sv/edu/catolica/parlessignes/Registro.java
@@ -1,15 +1,22 @@
 package sv.edu.catolica.parlessignes;
 
 import android.os.Bundle;
-import android.view.View;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
 
 public class Registro extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.registro);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
     }
 
 
