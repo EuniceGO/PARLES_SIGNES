260bdd7c6a7548e9495ca09fff4aa303ef505f4c
Rebeca Margarita Cruz Polanco <rebeca.cruz@catolica.edu.sv>
2025-05-30 00:39:17 -0600

    Agregando login

---
+++ app/src/main/java/sv/edu/catolica/parlessignes/LoginActivity.java
@@ -0,0 +1,12 @@
+package sv.edu.catolica.parlessignes;
+
+import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class LoginActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.login); // Asegúrate de que el XML se llama login.xml
+    }
+}
--- app/src/main/java/sv/edu/catolica/parlessignes/MainActivity.java
+++ app/src/main/java/sv/edu/catolica/parlessignes/MainActivity.java
@@ -3,7 +3,8 @@ package sv.edu.catolica.parlessignes;
 import static sv.edu.catolica.parlessignes.R.*;
 
 import android.os.Bundle;
-
+import android.view.View;
+import android.content.Intent;
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
@@ -22,5 +23,10 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+    }
+
+    public void Login(View view) {
+        Intent intent = new Intent(this, LoginActivity.class);
+        startActivity(intent);
     }
 }
