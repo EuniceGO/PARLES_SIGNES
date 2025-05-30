edcb3916bcb3daab59f4f91d119470772fcc9694
Katherine Gutierrez <katherine.gutierrez@catolica.edu.sv>
2025-05-30 01:06:19 -0600

    Enviando vista de registro y modificación de login

--- app/src/main/java/sv/edu/catolica/parlessignes/LoginActivity.java
+++ app/src/main/java/sv/edu/catolica/parlessignes/LoginActivity.java
@@ -1,6 +1,9 @@
 package sv.edu.catolica.parlessignes;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+
 import androidx.appcompat.app.AppCompatActivity;
 
 public class LoginActivity extends AppCompatActivity {
@@ -8,5 +11,10 @@ public class LoginActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.login); // Asegúrate de que el XML se llama login.xml
+    }
+
+    public void Registrarse(View view) {
+        Intent intent = new Intent(this, Registro.class);
+        startActivity(intent);
     }
 }
