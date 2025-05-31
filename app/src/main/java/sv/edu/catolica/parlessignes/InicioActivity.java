38fff5e70bc330c0ba7c0506318999a69d3d06b1
Katherine Gutierrez <katherine.gutierrez@catolica.edu.sv>
2025-05-30 23:07:23 -0600

    Creando vista de niveles

--- app/src/main/java/sv/edu/catolica/parlessignes/InicioActivity.java
+++ app/src/main/java/sv/edu/catolica/parlessignes/InicioActivity.java
@@ -1,6 +1,9 @@
 package sv.edu.catolica.parlessignes;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -21,4 +24,11 @@ public class InicioActivity extends AppCompatActivity {
             return insets;
         });
     }
+
+    public void Ver_niveles(View view) {
+
+        Intent intent = new Intent(this, NivelesActivity.class);
+        startActivity(intent);
+    }
+
 }
---
+++ app/src/main/java/sv/edu/catolica/parlessignes/NivelesActivity.java
@@ -0,0 +1,17 @@
+package sv.edu.catolica.parlessignes;
+
+import android.view.View;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+
+public class NivelesActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.niveles);
+    }
+
+}
