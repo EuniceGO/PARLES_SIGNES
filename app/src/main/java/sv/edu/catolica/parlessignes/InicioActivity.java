4416d2c753326a06c86e231978bcdf178bdad47c
Carolina Zepeda <mirian.hernandez1@catolica.edu.sv>
2025-05-30 21:56:31 -0600

    Enviando cambios del inico

--- app/src/main/java/sv/edu/catolica/parlessignes/InicioActivity.java
+++ app/src/main/java/sv/edu/catolica/parlessignes/InicioActivity.java
@@ -1,8 +1,6 @@
 package sv.edu.catolica.parlessignes;
 
-import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -10,26 +8,17 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class LoginActivity extends AppCompatActivity {
+public class InicioActivity extends AppCompatActivity {
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.login);
+        setContentView(R.layout.inicio);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
-    }
-
-    public void Registrarse(View view) {
-        Intent intent = new Intent(this, RegistroActivity.class);
-        startActivity(intent);
-    }
-
-    public void Inicio(View view) {
-        Intent intent = new Intent(this, InicioActivity.class);
-        startActivity(intent);
     }
 }
