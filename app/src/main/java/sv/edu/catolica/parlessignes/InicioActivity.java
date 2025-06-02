cbb4efa6cd38d74c2327b01c4d58f31dcc952270
Carolina Zepeda <mirian.hernandez1@catolica.edu.sv>
2025-06-02 00:31:12 -0600

    Enviando cambiso

--- app/src/main/java/sv/edu/catolica/parlessignes/InicioActivity.java
+++ app/src/main/java/sv/edu/catolica/parlessignes/InicioActivity.java
@@ -3,9 +3,7 @@ package sv.edu.catolica.parlessignes;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.MenuItem;
-import android.view.View;
 import android.widget.Button;
-import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
